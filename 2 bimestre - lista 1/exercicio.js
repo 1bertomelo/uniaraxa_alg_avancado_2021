74cf02223c55d9794e50c47987998f57c8c452db
1bertomelo <1bertomelo@gmail.com>
2021-05-13 20:30:36 -0300

    correcao exercicio ts

---
+++ 2 bimestre - lista 1/exercicio.js
@@ -0,0 +1,48 @@
+var inputPeso = document.getElementById('txtPeso');
+var inputAltura = document.getElementById('txtAltura');
+var inputImc = document.getElementById('txtImc');
+var buttonCalcImc = document.getElementById('btnCalcImc');
+buttonCalcImc.addEventListener('click', function () {
+    var resultadoImc = calcularImc(Number(inputPeso.value), Number(inputAltura.value));
+    console.log(resultadoImc);
+    inputImc.value = resultadoImc.toFixed(2).toString();
+});
+//sintaxe de criar uma função
+//function nomedaFuncao (parametros de entrada)
+//parametros de entrada:
+//nome do parametro1: tipo dele
+//nome do parametro2: tipo dele
+//...
+//funcao sem parametro nomeDela()
+function calcularImc(peso, altura) {
+    return peso / (Math.pow(altura, 2));
+}
+//conversao de tipo de dado
+// Number -> String : Exemplo peso.toString();
+// String -> Number : Exemplo  let x : string = "10"   Number(x);
+var inputNome = document.getElementById('txtNome');
+var inputQtdeVogais = document.getElementById('txtQtdVogais');
+var buttonContarVogais = document.getElementById('btnContarVogais');
+buttonContarVogais.addEventListener('click', function () {
+    var resultadoVogais = contarVogais(inputNome.value);
+    inputQtdeVogais.value = resultadoVogais.toString();
+});
+function contarVogais(nome) {
+    var qtdVogais = 0;
+    var vogais = 'aeiou';
+    for (var i = 0; i < nome.length; i++) {
+        /* if(nome[i] === 'a' ||
+            nome[i] === 'e' ||
+            nome[i] === 'i' ||
+            nome[i] === 'o' ||
+            nome[i] === 'u'
+         ){
+             qtdVogais++;
+         }*/
+        //outro jeito de fazer
+        if (vogais.indexOf(nome[i]) !== -1) {
+            qtdVogais++;
+        }
+    }
+    return qtdVogais;
+}
---
+++ 2 bimestre - lista 1/exercicio.ts
@@ -0,0 +1,55 @@
+let inputPeso = document.getElementById('txtPeso') as HTMLInputElement;
+let inputAltura = document.getElementById('txtAltura') as HTMLInputElement;
+let inputImc = document.getElementById('txtImc') as HTMLInputElement;
+let buttonCalcImc = document.getElementById('btnCalcImc') as HTMLButtonElement;
+
+buttonCalcImc.addEventListener('click', function () {
+    let resultadoImc: number = calcularImc(Number(inputPeso.value), Number(inputAltura.value));
+    console.log(resultadoImc);
+    inputImc.value = resultadoImc.toFixed(2).toString();
+
+});
+//sintaxe de criar uma função
+//function nomedaFuncao (parametros de entrada)
+//parametros de entrada:
+//nome do parametro1: tipo dele
+//nome do parametro2: tipo dele
+//...
+//funcao sem parametro nomeDela()
+function calcularImc(peso: number, altura: number) {
+    return peso / (altura ** 2);
+}
+
+//conversao de tipo de dado
+// Number -> String : Exemplo peso.toString();
+// String -> Number : Exemplo  let x : string = "10"   Number(x);
+
+let inputNome = document.getElementById('txtNome') as HTMLInputElement;
+let inputQtdeVogais = document.getElementById('txtQtdVogais') as HTMLInputElement;
+let buttonContarVogais = document.getElementById('btnContarVogais') as HTMLButtonElement;
+
+buttonContarVogais.addEventListener('click', function () {
+    let resultadoVogais: number = contarVogais(inputNome.value)
+    inputQtdeVogais.value = resultadoVogais.toString();
+});
+
+function contarVogais(nome: string) {
+    let qtdVogais: number = 0;
+    const vogais: string = 'aeiou';
+    for (let i = 0; i < nome.length; i++) {
+        /* if(nome[i] === 'a' || 
+            nome[i] === 'e' ||
+            nome[i] === 'i' ||
+            nome[i] === 'o' ||
+            nome[i] === 'u' 
+         ){
+             qtdVogais++;
+         }*/
+        //outro jeito de fazer
+        if (vogais.indexOf(nome[i]) !== -1) {
+            qtdVogais++;
+        }
+    }
+
+    return qtdVogais;
+}
